dc7ffd6c7ca118193b4796cbb81796558a9035b1
Amil Masic <amil.masic@gmail.com>
2022-01-26 10:56:56 -0500

    made a custom hook to fetch, using useEffect and useState

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import {useForm} from './useForm';
 import { Hello } from './Hello';
+import {useFetch} from './useFetch';
 
 
 
@@ -10,12 +11,20 @@ function App() {
 
   const [showHello, setShowHello] = useState(true)
 
-  const [count, setCount] = useState(10);
-  const [count2, setCount2] = useState(20);
+  // const [count, setCount] = useState(10);
+  // const [count2, setCount2] = useState(20);
   const [values, handleChange] = useForm({email: "", password: ""})
+
+  const [count, setCount] = useState(0);
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+
 
   return (
     <div>
+      <div>{!data ? 'loading..' : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c + 1)}>Increment</button>
+
       <button onClick={() => setShowHello(!showHello)}>toggle</button>
       {showHello && <Hello />}
 
